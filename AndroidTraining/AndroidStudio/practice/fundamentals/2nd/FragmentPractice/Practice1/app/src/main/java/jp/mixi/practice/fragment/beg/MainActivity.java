9910036bc521f2b957065e959fa9ee88164b2673
TK_Ratina <taka4.kato@gmail.com>
2015-05-25 17:17:10 +0900

    2.Activityとfragment 　practice fragment1, 2

--- AndroidTraining/AndroidStudio/practice/fundamentals/2nd/FragmentPractice/Practice1/app/src/main/java/jp/mixi/practice/fragment/beg/MainActivity.java
+++ AndroidTraining/AndroidStudio/practice/fundamentals/2nd/FragmentPractice/Practice1/app/src/main/java/jp/mixi/practice/fragment/beg/MainActivity.java
@@ -2,15 +2,21 @@
 package jp.mixi.practice.fragment.beg;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // FragmentManager も、android.support.v4.app.FragmentManager を利用する
+        FragmentManager manager = getSupportFragmentManager();
+        // レイアウトから Fragment を見つけ出してインスタンスを得る
+        manager.findFragmentById(R.id.my_fragment);
+
     }
 
     @Override
